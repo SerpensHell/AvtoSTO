25c5d574fbc10bff7f0046c0399351e52b4e5acf
ПВ-41 Забелина Екатерина <katyazabelina1949@gmail.com>
2022-04-19 11:54:31 +0300

    Разделила версии доски для пк и мобильных устройств

--- src/vue/app/src/main.js
+++ src/vue/app/src/main.js
@@ -7,14 +7,24 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBell } from '@fortawesome/free-solid-svg-icons'
 import { faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons'
 import { faBars} from '@fortawesome/free-solid-svg-icons'
+import { faPlus} from '@fortawesome/free-solid-svg-icons'
+import { faPencil} from '@fortawesome/free-solid-svg-icons'
+import { faRubleSign} from '@fortawesome/free-solid-svg-icons'
+
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faBell)
 library.add(faMagnifyingGlass)
 library.add(faChevronLeft)
 library.add(faBars)
-
+library.add(faChevronDown)
+library.add(faChevronUp)
+library.add(faPlus)
+library.add(faPencil)
+library.add(faRubleSign)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 
--- src/vue/app/src/views/Board.vue
+++ src/vue/app/src/views/Board.vue
@@ -1,62 +1,33 @@
 <template>
     <div class="board">
-        <div class="tasks-list">
-            <div class="button column">
-                <div class="column">
-                    <div class="list-header">
-                        <h2>Нужно сделать</h2>
-                        <div class="task-composer">
-                            <a class="open-task-composer">
-                                <span class="icon-add"> <i class="fa fa-plus"></i></span>
-                                <span class="add-a-task">Добавить заказ</span>
-                            </a>
-                        </div>
+        <div class="tasks-list-pc" >
+            <div class="column-pc">
+                <div class="list-header-pc">
+                    <h2>Нужно сделать</h2>
+                    <div class="task-composer-pc">
+                        <a class="open-task-composer-pc">
+                            <span class="icon-add-pc"><font-awesome-icon icon="fa-solid fa-plus" /></span>
+                            <span class="add-a-task-pc">Добавить заказ</span>
+                        </a>
                     </div>
+                </div>
 
-                    <div class="tasks-field">
-                        <div class="task-card">
-                            <div class="task">
-                                <a>
-                                    <div class="task-content">
-                                        <div class="line1">
-                                            <div class="task-id"># 388 </div>
-                                            <div class="client-fio">Тупикин Н.С.</div>
-                                            <div class="edit"> <i class="fa fa-pencil"></i> </div>
-                                        </div>
-                                        <div class="car-brand">Audi G8</div>
-                                        <div class="plait"><div class="number"> A 777 AA </div> <div class="region"> 31 </div></div>
-                                        <div class="task-description">Замена колодок заднего двигателя</div>
-                                        <div class="line4">
-                                            <div class="master-fio">Забелин А.Н.</div>
-                                            <div class="price">100000 <i class="fa fa-ruble"></i> </div>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="column">
-                <div class="list-header">
-                    <h2>В процессе</h2>
-                </div>
-                <div class="tasks-field">
-                    <div class="task-card">
-                        <div class="task">
+                <div class="tasks-field-pc">
+                    <div class="task-card-pc">
+                        <div class="task-pc">
                             <a>
-                                <div class="task-content">
-                                    <div class="line1">
-                                        <div class="task-id"># 388 </div>
-                                        <div class="client-fio">Тупикин Н.С.</div>
-                                        <div class="edit"> <i class="fa fa-pencil"></i></div>
+                                <div class="task-content-pc">
+                                    <div class="line1-pc">
+                                        <div class="task-id-pc"># 388 </div>
+                                        <div class="client-fio-pc">Тупикин Н.С.</div>
+                                        <div class="edit-pc"> <font-awesome-icon icon="fa-solid fa-pencil" /></div>
                                     </div>
-                                    <div class="car-brand">Audi G8</div>
-                                    <div class="plait"><div class="number"> A 777 AA </div> <div class="region"> 31 </div></div>
-                                    <div class="task-description">Замена колодок заднего двигателя</div>
-                                    <div class="line4">
-                                        <div class="master-fio">Забелин А.Н.</div>
-                                        <div class="price">100000 <i class="fa fa-ruble"></i> </div>
+                                    <div class="car-brand-pc">Audi G8</div>
+                                    <div class="plait-pc"><div class="number-pc"> A 777 AA </div> <div class="region-pc"> 31 </div></div>
+                                    <div class="task-description-pc">Замена колодок заднего двигателя</div>
+                                    <div class="line4-pc">
+                                        <div class="master-fio-pc">Забелин А.Н.</div>
+                                        <div class="price-pc">100000 <font-awesome-icon icon="fa-solid fa-ruble-sign" /> </div>
                                     </div>
                                 </div>
                             </a>
@@ -65,26 +36,169 @@
                 </div>
             </div>
 
-            <div class="column">
-                <div class="list-header">
+            <div class="column-pc">
+                <div class="list-header-pc">
+                    <h2>В процессе</h2>
+                </div>
+                <div class="tasks-field-pc">
+                    <div class="task-card-pc">
+                        <div class="task-pc">
+                            <a>
+                                <div class="task-content-pc">
+                                    <div class="line1-pc">
+                                        <div class="task-id-pc"># 388 </div>
+                                        <div class="client-fio-pc">Тупикин Н.С.</div>
+                                        <div class="edit-pc"> <font-awesome-icon icon="fa-solid fa-pencil" /></div>
+                                    </div>
+                                    <div class="car-brand-pc">Audi G8</div>
+                                    <div class="plait-pc"><div class="number-pc"> A 777 AA </div> <div class="region-pc"> 31 </div></div>
+                                    <div class="task-description-pc">Замена колодок заднего двигателя</div>
+                                    <div class="line4-pc">
+                                        <div class="master-fio-pc">Забелин А.Н.</div>
+                                        <div class="price-pc">100000 <font-awesome-icon icon="fa-solid fa-ruble-sign" /> </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="column-pc">
+                <div class="list-header-pc">
                     <h2>Готово</h2>
                 </div>
-                <div class="tasks-field">
-                    <div class="task-card">
-                        <div class="task">
+                <div class="tasks-field-pc">
+                    <div class="task-card-pc">
+                        <div class="task-pc">
                             <a>
-                                <div class="task-content">
-                                    <div class="line1">
-                                        <div class="task-id"># 388 </div>
-                                        <div class="client-fio">Тупикин Н.С.</div>
-                                        <div class="edit"> <i class="fa fa-pencil"></i></div>
+                                <div class="task-content-pc">
+                                    <div class="line1-pc">
+                                        <div class="task-id-pc"># 388 </div>
+                                        <div class="client-fio-pc">Тупикин Н.С.</div>
+                                        <div class="edit-pc"> <font-awesome-icon icon="fa-solid fa-pencil" /></div>
                                     </div>
-                                    <div class="car-brand">Audi G8</div>
-                                    <div class="plait"><div class="number"> A 777 AA </div> <div class="region"> 31 </div></div>
-                                    <div class="task-description">Замена колодок заднего двигателя</div>
-                                    <div class="line4">
-                                        <div class="master-fio">Забелин А.Н.</div>
-                                        <div class="price">100000 <i class="fa fa-ruble"></i> </div>
+                                    <div class="car-brand-pc">Audi G8</div>
+                                    <div class="plait-pc"><div class="number-pc"> A 777 AA </div> <div class="region-pc"> 31 </div></div>
+                                    <div class="task-description-pc">Замена колодок заднего двигателя</div>
+                                    <div class="line4-pc">
+                                        <div class="master-fio-pc">Забелин А.Н.</div>
+                                        <div class="price-pc">100000 <font-awesome-icon icon="fa-solid fa-ruble-sign" /> </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="tasks-list-mobile" >
+            <div class="button-column-mobile">
+                <div class="button-name-mobile"> <h2>Нужно сделать</h2> </div>
+                <div class="column-open-btn-mobile">
+                    <button @click="toggleElement1"> <font-awesome-icon icon="fa-solid fa-chevron-down" /> </button>
+                </div>
+            </div>
+                <div class="column-mobile" v-if="isOpen1">
+                    <div class="list-header-mobile">
+                        <h2>Нужно сделать</h2>
+                        <div class="task-composer-mobile">
+                            <a class="open-task-composer-mobile">
+                                <span class="icon-add-mobile"><font-awesome-icon icon="fa-solid fa-plus" /></span>
+                                <span class="add-a-task-mobile">Добавить заказ</span>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="tasks-field-mobile">
+                        <div class="task-card-mobile">
+                            <div class="task-mobile">
+                                <a>
+                                    <div class="task-content-mobile">
+                                        <div class="line1-mobile">
+                                            <div class="task-id-mobile"># 388 </div>
+                                            <div class="client-fio-mobile">Тупикин Н.С.</div>
+                                            <div class="edit-mobile"><font-awesome-icon icon="fa-solid fa-pencil" /></div>
+                                        </div>
+                                        <div class="car-brand-mobile">Audi G8</div>
+                                        <div class="plait-mobile"><div class="number-mobile"> A 777 AA </div> <div class="region-mobile"> 31 </div></div>
+                                        <div class="task-description-mobile">Замена колодок заднего двигателя</div>
+                                        <div class="line4-mobile">
+                                            <div class="master-fio-mobile">Забелин А.Н.</div>
+                                            <div class="price-mobile">100000 <font-awesome-icon icon="fa-solid fa-ruble-sign" /> </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            <div class="button-column-mobile">
+                <div class="button-name-mobile"> <h2>В процессе</h2> </div>
+                <div class="column-open-btn-mobile">
+                    <button> <font-awesome-icon icon="fa-solid fa-chevron-down" /> </button>
+                </div>
+            </div>
+            <div class="column-mobile" v-if="isOpen2">
+                <div class="list-header-mobile">
+                    <h2>В процессе</h2>
+                </div>
+                <div class="tasks-field-mobile">
+                    <div class="task-card-mobile">
+                        <div class="task-mobile">
+                            <a>
+                                <div class="task-content-mobile">
+                                    <div class="line1-mobile">
+                                        <div class="task-id-mobile"># 388 </div>
+                                        <div class="client-fio-mobile">Тупикин Н.С.</div>
+                                        <div class="edit-mobile"> <font-awesome-icon icon="fa-solid fa-pencil" /></div>
+                                    </div>
+                                    <div class="car-brand-mobile">Audi G8</div>
+                                    <div class="plait-mobile"><div class="number-mobile"> A 777 AA </div> <div class="region-mobile"> 31 </div></div>
+                                    <div class="task-description-mobile">Замена колодок заднего двигателя</div>
+                                    <div class="line4-mobile">
+                                        <div class="master-fio-mobile">Забелин А.Н.</div>
+                                        <div class="price-mobile">100000 <font-awesome-icon icon="fa-solid fa-ruble-sign" /> </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="button-column-mobile">
+                <div class="button-name-mobile"> <h2>Готово</h2> </div>
+                <div class="column-open-btn-mobile">
+                    <button> <font-awesome-icon icon="fa-solid fa-chevron-down" /> </button>
+                </div>
+            </div>
+            <div class="column-mobile" v-if="isOpen3">
+                <div class="list-header-mobile">
+                    <h2>Готово</h2>
+                </div>
+                <div class="tasks-field-mobile">
+                    <div class="task-card-mobile">
+                        <div class="task-mobile">
+                            <a>
+                                <div class="task-content-mobile">
+                                    <div class="line1-mobile">
+                                        <div class="task-id-mobile"># 388 </div>
+                                        <div class="client-fio-mobile">Тупикин Н.С.</div>
+                                        <div class="edit-mobile"> <font-awesome-icon icon="fa-solid fa-pencil" /></div>
+                                    </div>
+                                    <div class="car-brand-mobile">Audi G8</div>
+                                    <div class="plait-mobile"><div class="number-mobile"> A 777 AA </div> <div class="region-mobile"> 31 </div></div>
+                                    <div class="task-description-mobile">Замена колодок заднего двигателя</div>
+                                    <div class="line4-mobile">
+                                        <div class="master-fio-mobile">Забелин А.Н.</div>
+                                        <div class="price-mobile">100000 <font-awesome-icon icon="fa-solid fa-ruble-sign" /> </div>
                                     </div>
                                 </div>
                             </a>
@@ -95,5 +209,30 @@
         </div>
     </div>
 </template>
+
+<script>
+export default {
+  name: "Board",
+
+  data(){
+    return {
+        isOpen1: false,
+        isOpen2: false,
+        isOpen3: false,
+        }
+    },
+    methods: {
+        toggleElement1() {
+            this.isOpen1 = !this.isOpen1
+        },
+        toggleElement2() {
+            this.isOpen2 = !this.isOpen2
+        },
+        toggleElement3() {
+            this.isOpen3 = !this.isOpen3
+        }
+    }
+  }
+ </script>
 
 <style src="../assets/style/board_style.css"></style>
